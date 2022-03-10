48091b7817539f567443fbbe49407cd00ec66b75
PARK HYE WON <97939232+heong416@users.noreply.github.com>
2022-03-11 01:19:58 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    //슬라이더
+    
+    $('.mainSlider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots:true,
+        prevArrow:false,
+        nextArrow:false,
+        autoplay: true,
+        autoplaySpeed: 4000,
+	});
+    
+    $('.con01List').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots:true,
+        prevArrow:false,
+        nextArrow:false,
+        infinite:true,
+	});
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+})
